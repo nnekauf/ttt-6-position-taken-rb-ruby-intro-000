8c2ddece8e94e055cff904f8886ad5be148f1f5d
industrious-chip-8083 <ufumakanneka@gmail.com>
2020-10-27 02:48:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -12,8 +12,10 @@ end
 board = [" "," "," "," "," "," "," "," "," "]
 
 def position_taken?(board,index)
-  if board[index]== " " ||
+  if board[index]== " " || board[index]== "" || board[index]==nil
     return false
+  else
+    return true
 end
 
   
